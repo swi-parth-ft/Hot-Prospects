5ba5505b74d9e38636f8cada3127d5ad25857206
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-18 10:42:46 -0400

    control image interpolation

--- Hot Prospects/ContentView.swift
+++ Hot Prospects/ContentView.swift
@@ -9,35 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let users = ["Tohru", "Yuki", "Kyo", "Momiji"]
-    @State private var selection = Set<String>()
-    @State private var output = ""
-    
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
+        Image(.example)
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .background(.black)
     }
     
-    func fetchReadings() async {
-        let fetchTask = Task {
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings"
-        }
-        
-        let result =  await fetchTask.result
-        
-        switch result {
-        case .success(let message):
-            output = message
-        case .failure(let error):
-            output = "Error: \(error.localizedDescription)"
-        }
-        
-    }
+   
 }
 
 #Preview {
