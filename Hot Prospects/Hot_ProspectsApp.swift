b2e5a481e52b8668dfef646f3ede9040a19cc7a9
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-18 10:02:02 -0400

    Initial Commit

---
+++ Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Parth Antala on 2024-07-18.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
