28e9aafa81f00556e56e639d2ca1fcb369931756
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-18 11:05:07 -0400

    Scheduling local notifications

--- Hot Prospects/ContentView.swift
+++ Hot Prospects/ContentView.swift
@@ -5,26 +5,38 @@
 //  Created by Parth Antala on 2024-07-18.
 //
 
+import UserNotifications
 import SwiftUI
 
 struct ContentView: View {
     @State private var backgroundColor = Color.red
     
     var body: some View {
-        List {
-            Text("Elvis")
-                .swipeActions(edge: .leading) {
-                    Button("Send Message", systemImage: "message") {
-                        print("Hello")
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error {
+                        print(error.localizedDescription)
                     }
-                    .tint(.orange)
                 }
+            }
             
-                .swipeActions {
-                    Button("Delete", systemImage: "minus.circle", role: .destructive) {
-                        print("Deleting")
-                    }
-                }
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "it looks hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: "feedTheCat", content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+                
+                
+            }
         }
          
     }
