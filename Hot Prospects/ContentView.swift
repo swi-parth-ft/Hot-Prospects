d58d078eac5ed97226448df2d54fd43e9a4f86b6
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-18 11:12:44 -0400

    Adding Swift package dependencies

--- Hot Prospects/ContentView.swift
+++ Hot Prospects/ContentView.swift
@@ -7,36 +7,20 @@
 
 import UserNotifications
 import SwiftUI
+import SamplePackage
 
 struct ContentView: View {
     @State private var backgroundColor = Color.red
+    let possibleNumbers = 1...60
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.formatted()
+    }
     
     var body: some View {
         VStack {
-            Button("Request Permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                    if success {
-                        print("All set!")
-                    } else if let error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            
-            Button("Schedule Notification") {
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the cat"
-                content.subtitle = "it looks hungry"
-                content.sound = UNNotificationSound.default
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                let request = UNNotificationRequest(identifier: "feedTheCat", content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request)
-                
-                
-            }
+            Text(results)
         }
          
     }
