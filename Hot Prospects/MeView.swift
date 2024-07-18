9796499ba38017529a6e437ef2001e5b7bbc8a9e
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-18 12:27:55 -0400

    Add Context menu to Image

--- Hot Prospects/MeView.swift
+++ Hot Prospects/MeView.swift
@@ -11,7 +11,7 @@ struct MeView: View {
     
     @AppStorage("name") private var name = "Anonymous"
     @AppStorage("email") private var email = "you@yoursite.com"
-    
+    @State private var qrCode = UIImage()
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
     
@@ -27,14 +27,24 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRcode(from: "\(name)\n\(email)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        ShareLink(item: Image(uiImage: qrCode), preview: SharePreview("My QR Code", image: Image(uiImage: qrCode)))
+                    }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name, updateCode)
+            .onChange(of: email, updateCode)
         }
+    }
+    
+    func updateCode() {
+        qrCode = generateQRcode(from: "\(name)\n\(email)")
     }
     
     func generateQRcode(from string: String) -> UIImage {
@@ -43,6 +53,7 @@ struct MeView: View {
         if let outputImage = filter.outputImage {
             if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
                 return UIImage(cgImage: cgImage)
+               
             }
         }
         
