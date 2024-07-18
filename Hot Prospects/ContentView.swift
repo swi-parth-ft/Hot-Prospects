c33cdabce3b4d9cd2b16f9d3d28da8086a5be52a
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-18 10:39:05 -0400

    Understanding Swift’s Result type

--- Hot Prospects/ContentView.swift
+++ Hot Prospects/ContentView.swift
@@ -11,17 +11,32 @@ struct ContentView: View {
     
     let users = ["Tohru", "Yuki", "Kyo", "Momiji"]
     @State private var selection = Set<String>()
+    @State private var output = ""
     
     var body: some View {
-        List(users, id: \.self, selection: $selection) { user in
-            Text(user)
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+    
+    func fetchReadings() async {
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
         }
         
-        if !selection.isEmpty {
-            Text("You Selected \(selection.formatted())")
+        let result =  await fetchTask.result
+        
+        switch result {
+        case .success(let message):
+            output = message
+        case .failure(let error):
+            output = "Error: \(error.localizedDescription)"
         }
         
-        EditButton()
     }
 }
 
