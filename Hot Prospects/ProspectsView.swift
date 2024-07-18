2d9b3336dea190b5d1585b4ab327332c6483d900
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-18 11:41:39 -0400

    Dynamically filtering SwiftData query

--- Hot Prospects/ProspectsView.swift
+++ Hot Prospects/ProspectsView.swift
@@ -30,7 +30,15 @@ struct ProspectsView: View {
     
     var body: some View {
         NavigationStack {
-            Text("People: \(prospects.count)")
+            List(prospects) { prospect in
+                VStack(alignment: .leading) {
+                    Text(prospect.name)
+                        .font(.headline)
+                    
+                    Text(prospect.email)
+                        .foregroundStyle(.secondary)
+                }
+            }
                 .navigationTitle(title)
                 .toolbar {
                     Button("Scan", systemImage: "qrcode.viewfinder") {
@@ -38,6 +46,18 @@ struct ProspectsView: View {
                         modelContext.insert(prospect)
                     }
                 }
+        }
+    }
+    
+    init(filter: FilterType) {
+        self.filter = filter
+        
+        if filter != .none {
+            let showContactedOnly = filter == .contacted
+            
+            _prospects = Query(filter: #Predicate {
+                $0.isContacted == showContactedOnly
+            }, sort: [SortDescriptor(\Prospect.name)])
         }
     }
 }
